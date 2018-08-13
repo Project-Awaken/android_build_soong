76963fcc3c9137bb2c5876598f68791cb5419e82
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-13 11:03:48 -0700

    soong: Add AWAKEN_VERSION to dumpvars
    Change-Id: I24b29442f041aec451a173db404f542060ac67d2

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -137,6 +137,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 
 // Variables to print out in the top banner
 var BannerVars = []string{
+	"AWAKEN_VERSION",
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
 	"TARGET_PRODUCT",
